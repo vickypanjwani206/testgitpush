215e3691e8dccbee609899d86319ef1ed56c1fb5
vickypanjwani206 <57681269+vickypanjwani206@users.noreply.github.com>
2024-08-25 14:14:46 +0530

    Update test.py

--- test.py
+++ test.py
@@ -1,17 +1,1 @@
-import pymongo
-import certifi
-
-client = pymongo.MongoClient("mongodb+srv://vicky:vicky9007@cluster0.tlyoc.mongodb.net/?retryWrites=true&w=majority",tlsCAFile=certifi.where())
-db = client.test
-print(db)
-
-d={
-    "name":"Bharat",
-    "emaiid":"vickypanjwani206@gmail.com",
-    "surname":"panjwani"
-}
-
-db1=client['mongotest']
-coll=db1['test1']
-coll.insert_one(d)
-
+print("Hi Vicky")
